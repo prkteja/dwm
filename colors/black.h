af12244534d8b758cf0e7f8f769c6007f5ba4e8b
Rohit <prkteja99@gmail.com>
2025-10-02 10:13:22 +0530

    add colors

---
+++ colors/black.h
@@ -0,0 +1,14 @@
+static const char *colors[][3]      = {
+	/*               fg				bg         border   */
+	[SchemeNorm]	= { "#999999",	"#000000",	"#333333"  },
+	[SchemeSel]		= { "#ffffff",	"#000000",	"#666666"  },
+	[SchemeUline]	= { "#aaaaaa",	"#000000",	"#005577"  }, // border color of floating windows
+	[SchemeUlineA]	= { "#ffffff",	"#000000",	"#005577"  },
+	[SchemeSymbol]	= { "#ffffff",	"#000000",	"#005577"  },
+	[SchemeTitle]	= { "#ffffff",	"#000000",	"#005577"  },
+	[SchemeCol1]	= { "#ffffff",	"#000000",	"#005577"  }, // button bar
+	[SchemeCol2]	= { "#ffffff",	"#000000",	"#005577"  }, // floating win title
+	[SchemeCol3]	= { "#ffffff",	"#000000",	"#005577"  },
+	[SchemeCol4]	= { "#aaaaaa",	"#000000",	"#005577"  },
+};
+
---
+++ colors/default.h
@@ -0,0 +1,13 @@
+static const char *colors[][3]      = {
+	/*               fg				bg         border   */
+	[SchemeNorm]	= { "#eeeeee",	"#000000",	"#444444"  },
+	[SchemeSel]		= { "#ffffff",	"#005577",	"#285577" },
+	[SchemeUline]	= { "#bbbbbb",	"#000000",	"#005577"  }, // border color of floating windows
+	[SchemeUlineA]	= { "#ffffff",	"#000000",	"#005577"  },
+	[SchemeSymbol]	= { "#eeeeee",	"#000000",	"#005577"  },
+	[SchemeTitle]	= { "#ffffff",	"#005577",	"#005577"  },
+	[SchemeCol1]	= { "#ffffff",	"#000000",	"#005577"  }, // button bar
+	[SchemeCol2]	= { "#ffffff",	"#000000",	"#005577"  }, // floating win title
+	[SchemeCol3]	= { "#ffffff",	"#000000",	"#005577"  },
+	[SchemeCol4]	= { "#bbbbbb",	"#000000",	"#005577"  },
+};
